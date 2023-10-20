177d705db8b57bc86c8c58a49c7cd69c1234eec0
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-10-20 12:09:38 +0530

    init commit

--- app/src/main/java/com/example/sensor/MainActivity.kt
+++ app/src/main/java/com/example/sensor/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sensor
 
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,21 +11,33 @@ import android.widget.Toast
 import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sensorEventListener: SensorEventListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val sensorManager = getSystemService<SensorManager>()
-        if (sensorManager == null) {
-            Toast.makeText(this, "Could not process", Toast.LENGTH_LONG).show()
-            finish()
-        } else {
-            val sensor = sensorManager.getSensorList(Sensor.TYPE_ALL)
-            sensor.forEach {
-                Log.d("SENSOR", """
-                    ${it.name} | ${it.stringType} | ${it.vendor}
-                    """.trimIndent())
+//        if (sensorManager == null) {
+//            Toast.makeText(this, "Could not process", Toast.LENGTH_LONG).show()
+//            finish()
+//        } else {
+//            val sensor = sensorManager.getSensorList(Sensor.TYPE_ALL)
+//            sensor.forEach {
+//                Log.d("SENSOR", """
+//                    ${it.name} | ${it.stringType} | ${it.vendor}
+//                    """.trimIndent())
+//            }
+//        }
+        val proxySensor = sensorManager?.getSensorList(Sensor.TYPE_PROXIMITY)
+        sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("SENSOR", """onSensorChanged:${event!!.values[0]}""".trimIndent())
             }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                // do nothing in onAccuracyChanged function
+            }
+
         }
     }
 }
